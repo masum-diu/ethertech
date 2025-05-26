0d70c9a005abf21154f17e6cb4831c005574e3e3
“masum182” <masum.mollah@webable.digital>
2025-05-26 16:17:15 +0600

    added

---
+++ pages/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer className='bg-gray-800 text-white text-center p-4'>
+                <p>&copy; 2023 My Website</p></footer>
+        </div>
+    )
+}
+
+export default Footer
---
+++ pages/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+       <header className='bg-blue-600 text-white text-center p-4'>
+                <h1 className='text-2xl font-bold'>My Website</h1>
+            </header>
+    </div>
+  )
+}
+
+export default Header
