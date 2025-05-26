2a4deb9121a652ef3d4e6cda1e526c50d6fdd0e3
Mahmud-Saki6 <gazi.sakib12@northsouth.edu>
2025-05-26 16:26:47 +0600

    start new commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import Layout from './components/layout'
-
+import React from "react";
+import Layout from "./components/Layout";
 function home() {
   return (
     <Layout>
-      <div className='flex flex-col items-center justify-center h-screen bg-gray-100 text-red-500'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore nostrum quas vel ea animi, ipsam perspiciatis assumenda adipisci voluptate iste ex a eaque cum odio? Iure illum perferendis autem quos.
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-red-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore nostrum
+        quas vel ea animi, ipsam perspiciatis assumenda adipisci voluptate iste
+        ex a eaque cum odio? Iure illum perferendis autem quos.
       </div>
     </Layout>
-  )
+  );
 }
 
-export default home
+export default home;
